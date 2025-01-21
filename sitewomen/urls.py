4f8a45e482b39e4fb172ec7114add50c708eb8d8
Ytockai <kolia.boyackov@yandex.ru>
2025-01-21 09:44:03 +0300

    Добавил заглушку для авторизации

--- sitewomen/urls.py
+++ sitewomen/urls.py
@@ -25,6 +25,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
+    path('users/', include('users.urls', namespace="users")),
 ] + debug_toolbar_urls()
 
 if settings.DEBUG:
