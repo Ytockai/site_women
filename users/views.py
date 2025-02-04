6bc729f213c69c90bed397dc45224aa6e8910e37
Ytockai <kolia.boyackov@yandex.ru>
2025-02-04 10:59:54 +0300

    реализация смены пароля

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
@@ -7,7 +7,7 @@ from django.views.generic import CreateView, UpdateView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import LoginUserForm, ProfileUserForm, RegisterUserForm
+from .forms import LoginUserForm, ProfileUserForm, RegisterUserForm, PasswordChangeForm, UserPasswordChangeForm
 
 
 class loginUser(LoginView):
@@ -37,3 +37,9 @@ class ProfileUser(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+    
+
+class UserPasswordChange(PasswordChangeView):
+	form_class = UserPasswordChangeForm
+	success_url = reverse_lazy('users:password_change_done')
+	template_name = "users/password_change_form.html"
